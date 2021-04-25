406f4eda172d63cfff1fcec4bc3b145e8dae2e09
deil118 <deil118@mail.ru>
2021-04-25 19:23:43 +0300

    v1.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
-
-
-
 const Header = () => {
     return (
 
         <header className='header'>
             <img src='https://static.vecteezy.com/system/resources/previews/000/602/725/original/vector-silver-eagle-logo.jpg'></img>
+            <div>Hello</div>
         </header>
+        
     )
 }
 
